423bc7f09e939e584eb945efd920ce416c257bd4
pratik-narwade <2023bit053@sggs.ac.in>
2025-04-09 00:01:50 +0530

    Add files via upload

---
+++ Calculator_12/src/main/java/com/mycompany/calculator_12/Calculator_12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator_12;
+
+/**
+ *
+ * @author piyus
+ */
+public class Calculator_12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
